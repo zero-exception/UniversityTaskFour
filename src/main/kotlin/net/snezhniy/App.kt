9fd26298936c4c344d2d05bae88f0f1583d10949
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-10-31 14:24:03 +0400

    rewrote App into kotlin

--- src/main/kotlin/net/snezhniy/App.kt
+++ src/main/kotlin/net/snezhniy/App.kt
@@ -6,6 +6,21 @@
 
 package net.snezhniy
 
-fun main() {
-    println("hello from kotlin!")
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.declaredFunctions
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    val arg = if (args.isNotEmpty()) args[0] else "1"
+    val solutionClassPath = "net.snezhniy.tasks.task$arg.Main"
+
+    val cls = try {
+        Class.forName(solutionClassPath).kotlin
+    } catch (e: ClassNotFoundException) {
+        println("Вы указали номер задания, решение которого не найдено. Попробуйте снова.")
+        exitProcess(1)
+    }
+    val obj = cls.createInstance()
+    val method = cls.declaredFunctions.find { it.name == "run" }!!
+    method.call(obj)
 }
