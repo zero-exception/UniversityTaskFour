0c65f2d2c7f9ca73926230a2814a1da1281d5fb3
snezhniy <72914371+snezhniy@users.noreply.github.com>
2021-11-01 00:09:51 +0400

    solution 8

---
+++ src/main/kotlin/net/snezhniy/solutions/solution8/Main.kt
@@ -0,0 +1,31 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package net.snezhniy.solutions.solution8
+
+import net.snezhniy.solutions.SolutionBase
+import java.io.File
+import kotlin.system.exitProcess
+
+/*
+№8. Текстовый файл содержит произвольное количество строк. Каждая строка
+– это слова, разделенные пробелами. Подсчитать количество слов в файле.
+*/
+class Main : SolutionBase {
+    override fun run() {
+        print("Укажите путь к текстовому файлу: ")
+        val input = readLine().orEmpty().trim()
+
+        val f = File(input)
+        if (!f.exists()) {
+            println("Указанный файл не существует!")
+            exitProcess(1)
+        }
+
+        val count = f.readText().split("\\s+".toRegex()).size
+        println("Количество слов: $count")
+    }
+}
